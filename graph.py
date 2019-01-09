3f44d40123fbf48db4715149a69ca8d360eda34d
Sawyer Welden <sawyer.welden@gmail.com>
2019-01-09 17:49:33 -0500

    continuing redo

--- graph.py
+++ graph.py
@@ -1,13 +1,38 @@
 from graphics import *
+import graphics_objects as g
+import itertools
 
 # Redoing graphics because it became a software engineering mess
+win = None # have to have this at global level to use zelle graphics
 width = 400
 height = 400
-real_space =  [[ None for j in range(width)] for i in range(height)]
+real_space =  [[ 'black' for j in range(width)] for i in range(height)]
+cur_color = 'green'
 zoom_level = 400 # zoom at 400, 200, 100?
+screen_objects = g.group() # the objects we want displayed
+
+def pt(coords):
+    x,y = coords
+    real_space[x][y] = cur_color
 
 def draw_function():
-    pass
+    pt((10,10))
+    my_line = g.line((10,10), (10, 100))
+    my_square = g.square((200,200),50)
+    screen_objects.add_multiple([my_line, my_square])
+
+    for obj in screen_objects.obj_list:
+        # add draw them to the real_space buffer
+        for x,y in obj.point_list:
+                real_space[x][y] = cur_color
+        pass
+
+def real_space_to_pixel_space():
+    # TODO
+    for x,y in itertools.product(range(width), range(height)):
+        pixel = Point(x,y)
+        pixel.setFill(real_space[x][y])
+        pixel.draw(win)
 
 def main():
     print('Main')
@@ -15,6 +40,7 @@ def main():
     win = GraphWin("Graphics Window", width, height, autoflush=False)
     win.setBackground('black')
     draw_function()
+    real_space_to_pixel_space()
     win.getMouse()
     win.close()
     print('exited')
--- graphics_objects.py
+++ graphics_objects.py
@@ -1,1 +1,114 @@
-# graphics_object.py
+# graphics_objects.py
+
+class group():
+    def __init__(self):
+        self.obj_list = []
+
+    def add(self, a):
+        self.obj_list.append(a)
+
+    def add_multiple(self, obj_list):
+        for a in obj_list:
+            self.add(a)
+
+    def rm(self, a):
+        self.obj_list.remove(a)
+    
+    def __str__(self):
+        return 'Group:'+', '.join(str(a) for a in self.obj_list)
+
+class graphics_object():
+    def __init__(self, coords, color=None):
+        self.coords = coords
+        self.point_list = []
+        self.color = 'blue'
+
+    def __str__(self):
+        raise NotImplementedError
+
+class point(graphics_object):
+    # Not for use in othe grpahics objects
+    # Use this when you just want to draw 1 pixel
+    def __str__(self):
+        return 'point at {}'.format(self.coords)
+
+class line():
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+        self.point_list = self.bressenhelm(p1, p2)
+        
+    def bressenhelm(self, start, end):
+        x0,y0 = start
+        x1,y1 = end
+        dx = x1 - x0
+        dy = y1 - y0
+        pts = []
+    
+        # If slope > 1, we rotate
+        rotated = False
+        if (abs(dy) > abs(dx)):
+            rotated = True
+            x0,y0 = y0,x0
+            x1,y1 = y1,x1
+        
+        # Lines go left to right
+        # If they dont, swap the points
+        if x0 > x1:
+            x0,x1 = x1,x0
+            y0,y1 = y1,y0
+        dx = x1 - x0
+        dy = y1 - y0
+        err = int(dx / 2.0)
+        y_inc = 1 if y0 < y1 else -1
+        
+        y = y0
+        for x in range(x0, x1 + 1):
+            # for i in range(stroke_w):
+            # i would be added to y in each
+            point = (y, x) if rotated else (x, y)
+                # pt(point)
+            pts.append(point)
+            err -= abs(dy)
+            if err < 0:
+                y += y_inc
+                err += dx
+
+        return pts
+
+    def __str__(self):
+        return 'Line from {} to {}'.format(self.p1, self.p2)
+
+class square(graphics_object):
+    # super.coords defines the top left corner
+    def __init__(self, coords, side_length):
+        self.coords = coords
+        self.side_length = side_length
+        self.point_list = self.generate_pointlist()
+
+    # List of points in real space
+    def generate_pointlist(self): 
+        li = []
+        x,y = self.coords # top left
+        s = self.side_length
+        li.extend(line((x,y),(x+s,y)).point_list) # top
+        li.extend(line((x,y),(x,y+s)).point_list)  # left
+        li.extend(line((x+s,y),(x+s,y+s)).point_list) # right
+        li.extend(line((x,y+s),(x+s,y+s)).point_list) # bottom
+        return li
+
+    def __str__(self):
+        return 'sqr'
+
+
+if __name__ == "__main__":
+    print('Main Thread')
+    my_obj = graphics_object((50,50))
+    my_point = point((10,10))
+    my_square = square((50,50), 50)
+    my_line = line((10,10), (10, 100))
+    
+    my_group = group()
+    my_group.add(my_square)
+    my_group.add(my_line)
+    print(my_group)
